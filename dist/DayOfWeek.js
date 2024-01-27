19babcc7340a7bad5488839e6bd8cd6741c756b3
jinpag <jinpag@gmail.com>
2024-01-28 00:07:09 +0900

    TypeScriptコンパイル実行

---
+++ dist/DayOfWeek.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DayOfWeek = void 0;
+var DayOfWeek;
+(function (DayOfWeek) {
+    DayOfWeek[DayOfWeek["Sunday"] = 1] = "Sunday";
+    DayOfWeek[DayOfWeek["Monday"] = 2] = "Monday";
+    DayOfWeek[DayOfWeek["Tuesday"] = 3] = "Tuesday";
+    DayOfWeek[DayOfWeek["Wednesday"] = 4] = "Wednesday";
+    DayOfWeek[DayOfWeek["Thursday"] = 5] = "Thursday";
+    DayOfWeek[DayOfWeek["Friday"] = 6] = "Friday";
+    DayOfWeek[DayOfWeek["Saturday"] = 7] = "Saturday";
+})(DayOfWeek || (exports.DayOfWeek = DayOfWeek = {}));
+//# sourceMappingURL=DayOfWeek.js.map
